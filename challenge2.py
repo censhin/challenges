fc9a4bc189dd30d526dc967fb033f37f305211ca
censhin <censhin@gmail.com>
2013-03-17 21:50:40 +0000

    changed the code to use wait_until from utils.py

--- challenge2.py
+++ challenge2.py
@@ -3,28 +3,17 @@
 import sys
 import pyrax
 import time
-
-def get_image(img_name):
-    img = [img for img in cs.images.list()
-        if img_name in img.name][0]
-    time.sleep(5)
-    return img
+from pyrax import utils
 
 pyrax.set_credential_file("/home/graft/.rackspace_cloud_credentials")
 cs = pyrax.cloudservers
 server = cs.servers.list()
 flavor = server[0].flavor['id']
 server[0].create_image("backup_image")
+clone_image = [img for img in cs.images.list()
+        if img_name in img.name][0]
 
-while True:
-    clone_image = get_image("backup_image")
-    if clone_image.status != 'ACTIVE':
-        sys.stdout.write("Status: %s" % clone_image.status)
-    if clone_image.progress != 100:
-        sys.stdout.write("Progress: %d%%" % clone_image.progress)
-        sys.stdout.flush()
-    else:
-        print "Creating server from image."
-        break
-
+print "Waiting for image to finish building."
+utils.wait_until(clone_image, "status", ['ACTIVE', 'ERROR'], interval=20, attempts=40, verbose=True)
+print "Creating server from image."
 clone = cs.servers.create("clone", clone_image.id, flavor)
