eecf588f4562edcc3cc31bfad14117f47fb65f93
censhin <censhin@gmail.com>
2013-03-17 22:33:49 +0000

    some updates

--- challenge2.py
+++ challenge2.py
@@ -7,11 +7,13 @@ pyrax.set_credential_file("/home/graft/.rackspace_cloud_credentials")
 cs = pyrax.cloudservers
 server = cs.servers.list()
 flavor = server[0].flavor['id']
-server[0].create_image("backup_image")
+img_name = raw_input("Input the image name: ")
+serv_name = raw_input("Input the server name: ")
+server[0].create_image(img_name)
 clone_image = [img for img in cs.images.list()
         if img_name in img.name][0]
 
 print "Waiting for image to finish building."
 pyrax.utils.wait_until(clone_image, "status", ['ACTIVE', 'ERROR'], interval=20, attempts=40, verbose=True)
-print "Creating server from image."
-clone = cs.servers.create("clone", clone_image.id, flavor)
+print "\nCreating server from image."
+clone = cs.servers.create(serv_name, clone_image.id, flavor)
