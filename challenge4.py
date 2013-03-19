0722a15654da1c8ab16991ac80dbf1ad407b4642
censhin <censhin@gmail.com>
2013-03-19 17:30:44 +0000

    fixed error checking

--- challenge4.py
+++ challenge4.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import re
 import sys
 import pyrax
 
@@ -8,14 +9,15 @@ if len(sys.argv) == 3:
     creds_file = os.path.expanduser("~/.rackspace_cloud_credentials")
     pyrax.set_credential_file(creds_file)
     dns = pyrax.cloud_dns
-    domain = [dom for dom in dns.list()
-        if dom.name in sys.argv[1]][0]
-    if not domain:
-        name = raw_input("Enter a domain name: ")
+    name = re.split("^[^\.]+.", sys.argv[1])[1]
+    try:
+        domain = [dom for dom in dns.list()
+            if name in dom.name][0]
+    except IndexError:
+        print "DNS name: %s" % name
         email = raw_input("Enter an e-mail address: ")
         domain = dns.create(name=name, emailAddress=email)
-    record = {"type": "A", "name": sys.argv[1], "data":sys.argv[2], "ttl":1000}
+    record = {"type": "A", "name": name, "data":sys.argv[2], "ttl":1000}
     print domain.add_records(record)
 else:
     print "Usage: %s domain.com 123.45.678.9" % sys.argv[0]
-
