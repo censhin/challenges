1e0097819080258b516e0ad6f774d6e6e2159fd0
censhin <censhin@gmail.com>
2013-03-30 00:41:02 +0000

    updated error checking for FQDN

--- challenge4.py
+++ challenge4.py
@@ -20,8 +20,33 @@ if len(sys.argv) == 3:
     pyrax.set_credential_file(creds_file)
     dns = pyrax.cloud_dns
 
-    # strip leading entries such as www. from FQDN
+    # ensure the FQDN is correct
+    prefix = sys.argv[1].split(".")[0]
     name = re.split("^[^\.]+.", sys.argv[1])[1]
+    if prefix != "www":
+        print "Warning: domain not prefixed with www."
+        while True:
+            answer = raw_input("Did you mean \"%s\" to be your prefix? (y/n): " % prefix)
+            answer.lower()
+            if answer == "y":
+                break
+            elif answer == "n":
+                prefix = raw_input("Enter your FQDN prefix: ")
+            else:
+                print "Enter y or n."
+
+    while True:
+        answer = raw_input("Is your FQDN \"%s\" correct? (y/n): " % (prefix + "." + name))
+        answer.lower()
+        if answer == "y":
+            fqdn = (prefix + "." + name)
+            break
+        elif answer == "n":
+            fqdn = raw_input("Enter the correct FQDN: ")
+            prefix = re.split("^[^\.]+.", fqdn)[0]
+            name = re.split("^[^\.]+.", fqdn)[1]
+        else:
+            print "Enter y or n."
 
     # verify the domain exists, create it if it doesn't
     try:
@@ -30,10 +55,17 @@ if len(sys.argv) == 3:
     except IndexError:
         print "DNS name: %s" % name
         email = raw_input("Enter an e-mail address: ")
+        print "Creating domain."
         domain = dns.create(name=name, emailAddress=email)
 
     # create and add the record
-    record = {"type": "A", "name": name, "data":sys.argv[2], "ttl":1000}
+    print "A Record"
+    record = {"type": "A", "name": name, "data": sys.argv[2], "ttl":1000}
+    print domain.add_records(record)
+    print "CNAME Record for FQDN"
+    record = {"type": "CNAME", "name": fqdn, "data": name, "ttl":1000}
     print domain.add_records(record)
 else:
-    print "Usage: %s domain.com 123.45.678.9" % sys.argv[0]
+    print "Usage: %s www.domain.com 123.45.678.9" % sys.argv[0]
+    print "FQDN should be prefixed"
+    print "eg. www.domain.com or admin.domain.com"
